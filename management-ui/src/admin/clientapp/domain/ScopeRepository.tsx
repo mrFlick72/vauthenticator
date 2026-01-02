70e0867a71baf15ce226035a4167d2fff97c1214
mrflick72 <valerio.vaudi@gmail.com>
2026-01-02 20:04:19 +0100

    scope reposiory fix

--- management-ui/src/admin/clientapp/domain/ScopeRepository.tsx
+++ management-ui/src/admin/clientapp/domain/ScopeRepository.tsx
@@ -1,12 +1,17 @@
-//to be refactored
+import { getIdpBaseUrl } from "../../../config/ConfigLoader";
+
 export async function findAllScopes(): Promise<string[]> {
-    let response = await fetch("/scopes",
+        const baseUrl = await getIdpBaseUrl()
+
+    let response = await fetch(`${baseUrl}/.well-known/openid-configuration`,
         {
             method: "GET",
             headers: {
                 'Accept': 'application/json'
             },
-            credentials: 'same-origin'
+            mode: "cors",
+            credentials: 'include'
         });
-    return await response.json() as string[]
+    const response_body = await response.json()
+    return response_body.scopes_supported as string[]
 }
