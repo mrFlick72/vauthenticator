edc822e31a9227ad89b6f01fa042876b5ab44b2b
mrflick72 <valerio.vaudi@gmail.com>
2026-04-06 22:06:25 +0200

    fix !! in VAuthenticatorPasswordEncoder sub classes

--- auth-server/src/main/kotlin/com/vauthenticator/server/password/domain/PasswordEncoder.kt
+++ auth-server/src/main/kotlin/com/vauthenticator/server/password/domain/PasswordEncoder.kt
@@ -1,9 +1,9 @@
 package com.vauthenticator.server.password.domain
 
-//TODO consider to revisit it due to spring enforce that the operation can return a nullable result
 interface VAuthenticatorPasswordEncoder {
     fun encode(password: String): String
 
     fun matches(password: String, encodedPassword: String): Boolean
 }
 
+class CredentialEncodingException(message: String) : RuntimeException(message)
