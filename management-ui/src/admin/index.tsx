9df72ec57ac7e0ec9ccbdc61293d6e9781b85511
mrflick72 <valerio.vaudi@gmail.com>
2026-04-14 22:41:40 +0200

    add loading spinner fix token validation via jkws and fix auto token refresh

--- management-ui/src/admin/index.tsx
+++ management-ui/src/admin/index.tsx
@@ -11,6 +11,9 @@ import KeyManagementPage from "./key/KeyManagementPage";
 import MailTemplatePage from "./communication/MailTemplatePage";
 import ClientAppListPage from "./clientapp/pages/clientAppList/ClientAppListPage";
 import ClientAppManagementPage from './clientapp/pages/clientAppManagement/ClientAppManagementPage';
+import {authenticationChecker} from "../auth/Authenticator";
+
+authenticationChecker()
 
 const VAuthenticatorAdminApp = () =>
     <HashRouter>
